ca53cd035ccc250a7f6e86579e73738e3f0e9774
Quaqmre <tatarmehmetakif@gmail.com>
2019-04-24 11:18:57 +0300

    C# Örnek Repo Initial

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace C__Örneklerim
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Func<int, bool> g = (x) => x > 5;
+            var list = Enumerable.Range(1, 30).ToList();
+            var hh = WWhere((t) => t > 15, list);
+            hh.ForEach((i) => System.Console.WriteLine(i));
+        }
+        static Func<Func<int, bool>, List<int>, List<int>> WWhere = (t, f) =>
+         {
+             List<int> List = new List<int>();
+             foreach (var u in f)
+             {
+                 if (t(u))
+                     List.Add(u);
+             }
+             return List;
+         };
+
+
+    }
+
+}
