8b2e5621421ebbecbdd98c734d62385045fe77fd
MEHMET AKIF TATAR <mehmet.tatar@consultant.turkcell.com.tr>
2020-01-22 16:38:09 +0300

    extendMetotTry

---
+++ emtyStringForeach.cs
@@ -0,0 +1,27 @@
+// using System;
+// using System.Collections;
+// using System.Collections.Generic;
+// using System.Diagnostics;
+// using System.Linq;
+// using System.Linq.Expressions;
+// using System.Text;
+// using System.Threading;
+// using System.Threading.Tasks;
+// using System.Timers;
+
+// namespace C__Örneklerim
+// {
+
+//     class Program
+//     {
+
+//         public static void Main()
+//         {
+//             List<int> x = new List<int>();
+//             string a = "";
+//             StringBuilder cc = new StringBuilder(a);
+//             System.Console.WriteLine(cc.ToString().Replace(' ', 's'));
+//             x.ForEach(q => System.Console.WriteLine("asd"));
+//         }
+//     }
+// }
---
+++ extendMethod.cs
@@ -0,0 +1,38 @@
+// using System;
+// using System.Collections;
+// using System.Collections.Generic;
+// using System.Diagnostics;
+// using System.Linq;
+// using System.Linq.Expressions;
+// using System.Text;
+// using System.Threading;
+// using System.Threading.Tasks;
+// using System.Timers;
+
+// namespace C__Örneklerim
+// {
+
+//     class Program
+//     {
+
+//         public static void Main()
+//         {
+//             List<int> x = new List<int>();
+//             string a = "";
+//             StringBuilder cc = new StringBuilder(a);
+//             System.Console.WriteLine(cc.ToString().Replace(' ', 's'));
+//             x.ForEach(q => System.Console.WriteLine("asd"));
+//         }
+//     }
+//     class extender
+//     {
+//         public int stmethodDeneme(int i)
+//         {
+//             return i;
+//         }
+//         public string stmethodDeneme(int i) // önemli metodun imzasındaki parametre , dönüş tipine göre metot extend edilemiyor.
+//         {
+//             return "as";
+//         }
+//     }
+// }
